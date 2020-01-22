f1907fa608b133a8f16b739aca716a9f7412fcc2
Aman Gupta <amangupta884036@gmail.com>
2020-01-22 13:08:19 +0530

    staticTut.cpp is Automatically committed

---
+++ ADS/Lab/Programs/staticTut.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+void demo()
+{ 
+    // static variable
+    static int count = 0;
+    cout << count << " ";
+     
+    // value is updated and
+    // will be carried to next
+    // function calls
+    count++;
+}
+ 
+int main()
+{
+    for (int i=0; i<5; i++)    
+        demo();
+    return 0;
+}
